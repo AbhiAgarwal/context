b24c9183b028a41135bd8b81722b19c46ae6515e
Abhi Agarwal <me@abhiagarwal.com>
2014-11-15 04:57:09 -0500

    Updating

--- nytimes/nytimes.py
+++ nytimes/nytimes.py
@@ -6,52 +6,51 @@ from flask import Flask, jsonify, render_template, url_for
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.cluster import KMeans
 
-def cluster_articles(reference_article, articles):
-    v = DictVectorizer(sparse=False)
-    dataset = []
-    
-    related_articles_inds = []
-    reference_keys = reference_article["keywords"]
-    num_reference_keys = len(reference_keys)
-    ref_keywords = {}
-    for key in reference_keys:
-        ref_keywords[key["value"]] = 1
-        
-    dataset.append(ref_keywords)
-    
-    for article in articles:
-        keywords = {}
-        keys = article["keywords"]
-        for key in keys:
-            keywords[key["value"]] = 1
-            
-        dataset.append(keywords)
-        
-    vectorized = v.fit_transform(dataset)  
-    #trim irrelevant data points: vectorize w.r.t. reference article keywords, vs all keywords.
-    trimmed_vectorized = []
-    for i in xrange(0, len(vectorized)):
-        trimmed_vectorized.append(vectorized[i][-num_reference_keys:])
-        
-    num_clusters = 3
-    km = KMeans(n_clusters = num_clusters, init='random', n_init=1, verbose=1)
-    km.fit(trimmed_vectorized)
-    reference_label = km.labels_[0]
-    print km.labels_
-        
-    for i in xrange(1, len(km.labels_)):
-       if (reference_label == km.labels_[i]):
-           related_articles_inds.append(i)
-    
-    related_articles_urls = []
-    for i in xrange(0, len(related_articles_inds)):
-       index = related_articles_inds[i] - 1
-       related_articles_urls.append(articles[index]["web_url"])
-    
-    return related_articles_urls
-
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 NYTimes_API_KEY = 'ca470e1e91b15a82cc0d4350b08a3c0b:14:70189328'
+
+def cluster_articles(reference_article, articles):
+	v = DictVectorizer(sparse=False)
+	dataset = []
+	related_articles_inds = []
+	reference_keys = reference_article["keywords"]
+	num_reference_keys = len(reference_keys)
+	ref_keywords = {}
+
+	for key in reference_keys:
+		ref_keywords[key["value"]] = 1
+    dataset.append(ref_keywords)
+
+	for article in articles:
+		keywords = {}
+		keys = article["keywords"]
+		for key in keys:
+			keywords[key["value"]] = 1
+            
+		dataset.append(keywords)
+        
+	vectorized = v.fit_transform(dataset)  
+    # trim irrelevant data points: vectorize w.r.t. reference article keywords, vs all keywords.
+	trimmed_vectorized = []
+	for i in xrange(0, len(vectorized)):
+		trimmed_vectorized.append(vectorized[i][-num_reference_keys:])
+        
+	num_clusters = 1 + len(articles)
+	km = KMeans(n_clusters = num_clusters, init='random', n_init=1, verbose=1)
+	km.fit(trimmed_vectorized)
+	reference_label = km.labels_[0]
+	print km.labels_
+
+	for i in xrange(1, len(km.labels_)):
+		if (reference_label == km.labels_[i]):
+			related_articles_inds.append(i)
+
+	related_articles_urls = []
+	for i in xrange(0, len(related_articles_inds)):
+		index = related_articles_inds[i] - 1
+		related_articles_urls.append(articles[index]["web_url"])
+
+	return related_articles_urls
 
 def getArticle(url):
 	# Beautiful Soup scraping for Article
