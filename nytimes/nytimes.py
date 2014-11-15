fbb4d6cbe3eca81f0e82593e3b0b260cf5419795
Abhi Agarwal <me@abhiagarwal.com>
2014-11-15 04:53:26 -0500

    Updating

--- nytimes/nytimes.py
+++ nytimes/nytimes.py
@@ -1,8 +1,54 @@
-import re, requests, json, urllib2, wikipedia
+import re, requests, json, urllib2, wikipedia, praw
 from nytimesarticle import articleAPI
 from bs4 import BeautifulSoup
 from cookielib import CookieJar
 from flask import Flask, jsonify, render_template, url_for
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.cluster import KMeans
+
+def cluster_articles(reference_article, articles):
+    v = DictVectorizer(sparse=False)
+    dataset = []
+    
+    related_articles_inds = []
+    reference_keys = reference_article["keywords"]
+    num_reference_keys = len(reference_keys)
+    ref_keywords = {}
+    for key in reference_keys:
+        ref_keywords[key["value"]] = 1
+        
+    dataset.append(ref_keywords)
+    
+    for article in articles:
+        keywords = {}
+        keys = article["keywords"]
+        for key in keys:
+            keywords[key["value"]] = 1
+            
+        dataset.append(keywords)
+        
+    vectorized = v.fit_transform(dataset)  
+    #trim irrelevant data points: vectorize w.r.t. reference article keywords, vs all keywords.
+    trimmed_vectorized = []
+    for i in xrange(0, len(vectorized)):
+        trimmed_vectorized.append(vectorized[i][-num_reference_keys:])
+        
+    num_clusters = 3
+    km = KMeans(n_clusters = num_clusters, init='random', n_init=1, verbose=1)
+    km.fit(trimmed_vectorized)
+    reference_label = km.labels_[0]
+    print km.labels_
+        
+    for i in xrange(1, len(km.labels_)):
+       if (reference_label == km.labels_[i]):
+           related_articles_inds.append(i)
+    
+    related_articles_urls = []
+    for i in xrange(0, len(related_articles_inds)):
+       index = related_articles_inds[i] - 1
+       related_articles_urls.append(articles[index]["web_url"])
+    
+    return related_articles_urls
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 NYTimes_API_KEY = 'ca470e1e91b15a82cc0d4350b08a3c0b:14:70189328'
@@ -28,7 +74,7 @@ def getArticle(url):
 	allImages = []
 	for eachImage in img:
 		allImages.append(eachImage['src'])
-	currentArticle['allText'] = allText
+	currentArticle['allText'] = allText	
 	currentArticle['img'] = allImages
 
 	# Definition Engine
@@ -38,6 +84,8 @@ def getArticle(url):
 		currentArticle['keywords'][eachKeyword]['definition'] = currentDefinition
 
 	return currentArticle
+
+print cluster_articles(getArticle('http://www.nytimes.com/2014/11/14/world/middleeast/abu-bakr-baghdadi-islamic-state-leader-calls-for-new-fight-against-west.html'), [getArticle('http://www.nytimes.com/2014/11/15/sports/not-all-leagues-ready-to-go-all-in-on-legalized-gambling.html'), getArticle('http://www.nytimes.com/2014/11/15/business/some-retailers-are-promoting-their-decision-to-remain-closed-on-thanksgiving.html?hp&action=click&pgtype=Homepage&module=photo-spot-region&region=top-news&WT.nav=top-news'), getArticle('http://www.nytimes.com/2014/11/14/world/middleeast/abu-bakr-baghdadi-islamic-state-leader-calls-for-new-fight-against-west.html')])
 
 # Primary route
 @app.route("/")
